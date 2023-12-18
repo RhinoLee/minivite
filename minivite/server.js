6ef064f58bd3f4f2051127f03d8b58f9514eaff7
RhinoLee <picklerhino@gmail.com>
2023-12-18 15:47:56 +0800

    create WebSocket server

--- minivite/server.js
+++ minivite/server.js
@@ -1,15 +1,30 @@
 import http from "http"
 import color from "picocolors"
 import connect from "connect"
+import { WebSocketServer } from "ws"
 import { indexHTMLMiddleware } from "./middleware"
 
 const middleware = connect()
-const { PORT_HTTP, PROJECT_NAME } = process.env
+const { PORT_HTTP, PORT_WS, PROJECT_NAME } = process.env
+
+const createWSServer = () => {
+  const server = new WebSocketServer({ port: PORT_WS })
+
+  server.on("connection", (ws) => {
+    console.log(`${color.green(`WebSocket Connected!!!`)}`)
+
+    ws.on("message", (data) => {
+      console.log("Received: %s", data)
+    })
+  })
+}
 
 middleware.use(indexHTMLMiddleware)
 
 function createServer() {
   http.createServer(middleware).listen(PORT_HTTP)
+
+  createWSServer()
 
   console.log(
     `${color.red(PROJECT_NAME)} server ON! ${color.green(
